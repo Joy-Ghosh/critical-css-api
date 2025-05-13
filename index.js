640aaf488a48fc7cb32ce16b7ddfba48ef22bd67
Joy <joyoffice9832@gmail.com>
2025-05-13 13:17:40 +0530

    Add CORS support

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+app.use(cors());
 const penthouse = require('penthouse');
 const axios = require('axios');  // Import axios for fetching CSS
 const fs = require('fs');
